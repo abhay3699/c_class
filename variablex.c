d457d275d115e531c3c1a44ec53e3dbba1a1f0b2
abhay ranjan <68952958+abhay3699@users.noreply.github.com>
2020-12-12 10:08:10 +0530

    Add files via upload

---
+++ variablex.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int inum=6;
+    float fnum=7.91;
+    double dnum=10.23;
+    long lnum=9;
+    char cname='A';
+    char sname[6]="abhay";
+    printf("int=%d, float=%f, double=%lf, long=%ld, char=%c, string=%s", inum,fnum,dnum,lnum,cname,sname);
+    return 0;
+}
