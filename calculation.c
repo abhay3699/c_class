501ccd1a0188866d633a4e5c687ca9f30b6f77d1
abhay ranjan <68952958+abhay3699@users.noreply.github.com>
2020-12-15 13:37:48 +0530

    Add files via upload

---
+++ calculation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a,b;
+    printf("enter the value of a - ");
+    scanf("%d",&a);
+    printf("enter the value of b - ");
+    scanf("%d",&b);
+    printf("\naddition of a and b = %d",a+b);
+    printf("\nsubtraction of a and b = %d",a-b);
+    printf("\nmultiplication of a and b = %d",a*b);
+    printf("\ndivision of a and b = %d",a/b);
+    printf("\nremender of a and b = %d",a%b);
+    return 0;
+}
